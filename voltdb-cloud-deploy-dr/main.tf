13da72196f234e80d7e361a29403955f669a45c1
Simongao <redegao@gmail.com>
2019-04-30 10:53:33 -0400

    first commit

---
+++ voltdb-cloud-deploy-dr/main.tf
@@ -0,0 +1,167 @@
+provider "alicloud" {
+    #Replace the access keys with yours.
+	access_key = ""
+	secret_key = ""
+	region = "ap-northeast-1"
+}
+
+variable "node_count" {
+  default = "3"
+}
+
+variable "node_name_prefix" {
+  description = "Prefix to use when naming cluster members"
+  default = "test-node-"
+}
+
+variable "node2_name_prefix" {
+  description = "Prefix to use when naming cluster members"
+  default = "cluster2-node-"
+}
+
+variable "password" {
+  type = "string"
+  default = "Test1234!"
+}
+
+variable "availabe_zone" {
+  type = "string"
+  default = "ap-northeast-1a"
+}
+
+data "alicloud_instance_types" "4c16g" {
+  cpu_core_count = 8
+  memory_size = 8
+}
+
+resource "alicloud_vpc" "vpc" {
+  name       = "tf_test_foo"
+  cidr_block = "172.16.0.0/12"
+}
+
+resource "alicloud_vswitch" "vsw" {
+  vpc_id            = "${alicloud_vpc.vpc.id}"
+  cidr_block        = "172.16.0.0/21"
+  availability_zone = "${var.availabe_zone}"
+}
+
+
+resource "alicloud_security_group" "default" {
+  name = "default"
+  vpc_id = "${alicloud_vpc.vpc.id}"
+}
+
+resource "alicloud_security_group_rule" "allow_all_tcp" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "1/65535"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.default.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+resource "alicloud_instance" "test-ins" {
+  count = "${var.node_count}"
+  # cn-beijing
+  availability_zone = "${var.availabe_zone}"
+  security_groups = ["${alicloud_security_group.default.*.id}"]
+
+  # series III
+  instance_type        = "${data.alicloud_instance_types.4c16g.instance_types.0.id}"
+  system_disk_category = "cloud_efficiency"
+  image_id             = "ubuntu_18_04_64_20G_alibase_20190223.vhd"
+  instance_name        = "${var.node_name_prefix}-${count.index}"
+  host_name        = "${var.node_name_prefix}${count.index}"
+  vswitch_id = "${alicloud_vswitch.vsw.id}"
+  internet_max_bandwidth_out = 5
+  password = "${var.password}"
+
+}
+
+resource "null_resource" "configure-ins-ips" {
+  count = "${var.node_count}"
+
+  provisioner "file" {
+        source = "scripts/initialization_env.sh"
+        destination = "~/initialization_env.sh"
+
+        connection {
+          type     = "ssh"
+          user     = "root"
+          host = "${element(alicloud_instance.test-ins.*.public_ip, count.index)}"
+          password = "${var.password}"
+      }
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            # Adds all cluster members' IP addresses to /etc/hosts (on each member)
+            "sudo echo '${join("\n", formatlist("%v", alicloud_instance.test-ins.*.private_ip))}' | awk 'BEGIN{ print \"\\n\\n# Cluster members:\" }; { print $0 \" ${var.node_name_prefix}\" NR-1 }' | sudo tee -a /etc/hosts > /dev/null",
+            "sudo echo '${join("\n", formatlist("%v", alicloud_instance.cluster2.*.private_ip))}' | awk 'BEGIN{ print \"\\n\\n# Cluster members:\" }; { print $0 \" ${var.node2_name_prefix}\" NR-1 }' | sudo tee -a /etc/hosts > /dev/null",
+            "sudo chmod +x ~/initialization_env.sh",
+            "sudo ~/initialization_env.sh 1 master",
+            "/opt/voltdb/bin/voltdb start --host=${join(",", formatlist("%v", alicloud_instance.test-ins.*.host_name))} -B"
+
+        ]
+        connection {
+          type     = "ssh"
+          user     = "root"
+          host = "${element(alicloud_instance.test-ins.*.public_ip, count.index)}"
+          password = "${var.password}"
+      }
+    }
+}
+
+resource "alicloud_instance" "cluster2" {
+  count = "${var.node_count}"
+  # cn-beijing
+  availability_zone = "${var.availabe_zone}"
+  security_groups = ["${alicloud_security_group.default.*.id}"]
+
+  # series III
+  instance_type        = "${data.alicloud_instance_types.4c16g.instance_types.0.id}"
+  system_disk_category = "cloud_efficiency"
+  image_id             = "ubuntu_18_04_64_20G_alibase_20190223.vhd"
+  instance_name        = "${var.node2_name_prefix}-${count.index}"
+  host_name        = "${var.node2_name_prefix}${count.index}"
+  vswitch_id = "${alicloud_vswitch.vsw.id}"
+  internet_max_bandwidth_out = 5
+  password = "${var.password}"
+
+}
+
+resource "null_resource" "configure-ins2-ips" {
+  count = "${var.node_count}"
+
+  provisioner "file" {
+        source = "scripts/initialization_env.sh"
+        destination = "~/initialization_env.sh"
+
+        connection {
+          type     = "ssh"
+          user     = "root"
+          host = "${element(alicloud_instance.cluster2.*.public_ip, count.index)}"
+          password = "${var.password}"
+      }
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            # Adds all cluster members' IP addresses to /etc/hosts (on each member)
+            "sudo echo '${join("\n", formatlist("%v", alicloud_instance.test-ins.*.private_ip))}' | awk 'BEGIN{ print \"\\n\\n# Cluster members:\" }; { print $0 \" ${var.node_name_prefix}\" NR-1 }' | sudo tee -a /etc/hosts > /dev/null",
+            "sudo echo '${join("\n", formatlist("%v", alicloud_instance.cluster2.*.private_ip))}' | awk 'BEGIN{ print \"\\n\\n# Cluster members:\" }; { print $0 \" ${var.node2_name_prefix}\" NR-1 }' | sudo tee -a /etc/hosts > /dev/null",
+            "sudo chmod +x ~/initialization_env.sh",
+            "sudo ~/initialization_env.sh 2 replica \"${join(",", formatlist("%v", alicloud_instance.test-ins.*.host_name))}\"",
+            "/opt/voltdb/bin/voltdb start --host=${join(",", formatlist("%v", alicloud_instance.cluster2.*.host_name))} -B"
+
+        ]
+        connection {
+          type     = "ssh"
+          user     = "root"
+          host = "${element(alicloud_instance.cluster2.*.public_ip, count.index)}"
+          password = "${var.password}"
+      }
+    }
+}
---
+++ voltdb-cloud-deploy-dr/scripts/initialization_env.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#"echo 'hello from $HOST' > ~/terraform_complete",
+curl -L -o /opt/openjdk-11.0.2_linux-x64_bin.tar.gz -O https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
+mkdir /usr/local/open-java-11
+tar -zxf /opt/openjdk-11.0.2_linux-x64_bin.tar.gz -C /usr/local/open-java-11
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get update
+apt-get install -y python2.7
+ln -s /usr/bin/python2.7 /usr/local/bin/python
+
+for f in /sys/kernel/mm/*transparent_hugepage/enabled; do
+    if test -f $f; then echo never > $f; fi
+done
+for f in /sys/kernel/mm/*transparent_hugepage/defrag; do
+    if test -f $f; then echo never > $f; fi
+done
+
+echo -e '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                <license>
+                <permit version="1" scheme="0">
+                <type>Enterprise Edition</type>
+                <issuer>
+                <company>VoltDB</company>
+                <email>support@voltdb.com</email>
+                <url>http://voltdb.com/</url>
+                </issuer>
+                <issuedate>2019-01-09</issuedate>
+                <licensee>VoltDB Field Engineering</licensee>
+                <expiration>2020-01-09</expiration>
+                <hostcount max="200"/>
+                <features trial="false">
+                <wanreplication>true</wanreplication>
+                <dractiveactive>true</dractiveactive>
+                </features>
+                </permit>
+                <signature>
+                302C02147F6B637FC0267E4F46F4E4E704A41FB8FD44AD
+                4802146DD24AB72C167F63E069894C460F9028AE3559FF
+                </signature>
+                </license>' > ~/license.xml
+            
+echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>
+              <deployment>
+              <cluster sitesperhost=\"8\" kfactor=\"1\" />
+              <commandlog synchronous=\"false\" enabled=\"true\" logsize=\"10000\"/>
+              <snapshot enabled=\"false\"/>
+              <httpd enabled=\"true\">
+              <jsonapi enabled=\"true\" />
+              </httpd>
+              <systemsettings>
+              <temptables maxsize=\"1024\"/>
+              <query timeout=\"30000\"/>
+              </systemsettings>
+              <dr id=\"$1\" role=\"$2\" >
+                 <connection source=\"$3\" />
+              </dr>
+              </deployment>" > ~/deployment.xml
+
+curl -L -o /opt/voltdb-ent-9.0.tar.gz -O https://downloads.voltdb.com/technologies/server/voltdb-ent-9.0.tar.gz
+tar -xzvf /opt/voltdb-ent-9.0.tar.gz -C /opt/
+mv /opt/voltdb-ent-9.0 /opt/voltdb
+update-alternatives --install "/usr/bin/java" "java" "/usr/local/open-java-11/jdk-11.0.2/bin/java" 1500
+update-alternatives --install "/usr/bin/javac" "javac" "//usr/local/open-java-11/jdk-11.0.2/bin/javac" 1500
+update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/local/open-java-11/jdk-11.0.2/bin/javaws" 1500
+update-alternatives --install "/usr/bin/jps" "jps" "/usr/local/open-java-11/jdk-11.0.2/bin/jps" 1500
+echo -e 'export VOLT=/opt/voltdb' >> ~/.bashrc
+echo -e 'export PATH=$PATH:$VOLT/bin' >> ~/.bashrc
+echo -e 'export VOLTDB_HEAPMAX="2048"' >> ~/.bashrc
+mv -f ~/license.xml /opt/voltdb/voltdb/
+/opt/voltdb/bin/voltdb init -f -C ~/deployment.xml >> ~/voltdb_init.log
+export VOLTDB_HEAPMAX="2048"
+pip install -r /opt/voltdb/lib/python/voltsql/requirements.txt
+
+#Install Docker & K8s
+apt install -y docker.io
+systemctl enable docker
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+apt install -y software-properties-common
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt install -y kubeadm
+swapoff -a
